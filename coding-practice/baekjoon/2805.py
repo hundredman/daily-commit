aa5a84a6beaee416e54b5c58d403d5a8944fe901
hundredman <sieunsaemkim@gmail.com>
2025-09-19 01:44:29 +0900

    Solve Baekjoon 2805

---
+++ coding-practice/baekjoon/2805.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/2805
+# 나무 자르기
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+trees = list(map(int, sys.stdin.readline().split()))
+
+start = 0
+end = max(trees)
+
+result = 0
+while start <= end:
+    mid = (start + end) // 2
+    
+    total_cut = sum(t - mid for t in trees if t > mid)
+
+    if total_cut >= m:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(result)
