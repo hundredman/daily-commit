e1678f86c77f0677c36fc04f6daa395e0c517189
hundredman <sieunsaemkim@gmail.com>
2025-09-27 00:11:28 +0900

    Solve LeetCode 559

---
+++ coding-practice/leetcode/559.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
+# Maximum Depth of N-ary Tree - Depth-First Search
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        if not root.children:
+            return 1
+        
+        depth = 0
+        for child in root.children:
+            depth = max(depth, self.maxDepth(child))
+        
+        return depth + 1
