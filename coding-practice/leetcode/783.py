afc27f5cddaf8854ce969820af2499f8d6411d68
hundredman <sieunsaemkim@gmail.com>
2025-09-27 15:19:58 +0900

    Solve LeetCode 783

---
+++ coding-practice/leetcode/783.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/
+# Minimum Distance Between BST Nodes - Inorder Traversal
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right\
+
+class Solution:
+    def minDiffInBST(self, root) -> int:
+        stack = []
+        current = root
+        prev = None
+        min_diff = float('inf')
+        
+        while stack or current:
+            while current:
+                stack.append(current)
+                current = current.left
+            
+            current = stack.pop()
+            if prev is not None:
+                min_diff = min(min_diff, current.val - prev.val)
+            prev = current
+            current = current.right
+        
+        return min_diff
