cc01ab818ae174be77cc94d57fca2b9eaeccfc68
hundredman <sieunsaemkim@gmail.com>
2025-12-01 20:35:48 +0900

    Solve Baekjoon 12865

---
+++ coding-practice/baekjoon/12865.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/12865
+# 평범한 배낭 - Dynamic Programming
+
+import sys
+
+input = sys.stdin.readline
+N, K = map(int, input().split())
+items = [tuple(map(int, input().split())) for _ in range(N)]
+
+dp = [0] * (K + 1)
+
+for weight, value in items:
+    for j in range(K, weight - 1, -1):
+        dp[j] = max(dp[j], dp[j - weight] + value)
+
+print(dp[K])
